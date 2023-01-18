67bd32ef33b7d95c6cd6feb6d6309f9f4fecb1ae
berger <robertberger.dev@gmail.com>
2023-01-18 12:54:04 -0500

    setup iterator that counts sum of numList array and consol logs the total

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ let firstName = 'JimBob';
 let birthday = '01/07/1855';
 let individualID = `Name: ${firstName} | Date of Birth: ${birthday}`;
 console.log(individualID);
+let numList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numListTotal = 0;
+for (let value of numList) {
+    numListTotal = numListTotal + value;
+}
+console.log(numListTotal);
--- index.ts
+++ index.ts
@@ -1,6 +1,16 @@
 let firstName: string = 'JimBob';
 let birthday: string = '01/07/1855';
 
-let individualID: string = `Name: ${firstName} | Date of Birth: ${birthday}`
+let individualID: string = `Name: ${firstName} | Date of Birth: ${birthday}`;
 
 console.log(individualID);
+
+let numList: Array<number> = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+let numListTotal: number = 0;
+
+for (let value of numList){
+   numListTotal = numListTotal + value;
+}
+
+console.log(numListTotal);
