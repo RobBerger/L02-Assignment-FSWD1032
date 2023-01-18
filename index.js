827e6ac6f7c30e8823d17f298a57ecd2a803de79
berger <robertberger.dev@gmail.com>
2023-01-18 12:26:31 -0500

    tested let individualID

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+let firstName = 'JimBob';
+let birthday = '01/07/1855';
+let individualID = `Name: ${firstName} | Date of Birth: ${birthday}`;
+console.log(individualID);
--- index.ts
+++ index.ts
@@ -1,2 +1,6 @@
-let firstName: string = 'Jim Bob';
-let birthday: string = '01/07/1855'
+let firstName: string = 'JimBob';
+let birthday: string = '01/07/1855';
+
+let individualID: string = `Name: ${firstName} | Date of Birth: ${birthday}`
+
+console.log(individualID);
